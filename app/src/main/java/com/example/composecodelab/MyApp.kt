88072ef9b8554035169736f460792983786c2641
consultant <Philippe.cherlan-miche@techconsulting.tech>
2023-07-14 08:08:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/composecodelab/MyApp.kt
@@ -0,0 +1,7 @@
+package com.example.composecodelab
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
